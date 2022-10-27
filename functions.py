9cb4e74e4ad8a2487fe60588cdcdca8cc3d1d431
will <zhenlanwang0426@gmail.com>
2022-10-26 21:23:38 -0400

    Add files via upload

--- functions.py
+++ functions.py
@@ -6,26 +6,30 @@ Created on Tue Oct 25 11:04:17 2022
 @author: will
 """
 import numpy as np
-matrix = np.load('matrix.npy')
+matrix = np.load('/home/will/Desktop/LC/wordle/matrix.npy')
 mapping = dict()
-
+#l = []
 def entropy(p):
     return np.sum(-p*np.log2(p))
     
-def wordle(matrix,index,top=0.5,count=2309):
+def wordle(matrix,index,top=0.6,count=2309):
     # return min expected guesses
-    d = matrix.shape[1]
-    if d == 1: return 0
-    if d == 2: return 1
+    if count == 1: return 0
+    if count == 2: return 1
     if tuple(index) in mapping:
         return mapping[tuple(index)]
-    threshold = np.log2(count) * top
+    best = np.log2(count)
+    threshold = best * top
     min_ = 1000
     for row in range(12953):
         unq,unqtags,counts = np.unique(matrix[row],return_inverse=True,return_counts=True)
         ps = counts/count
         entro = entropy(ps)
-        if entro>threshold:
+        if entro == best:
+            mapping[tuple(index)] = 1
+            return 1
+        elif entro>threshold:
+            #l.append((matrix.shape[1],entro))
             guess_row = 1
             for p,u,c in zip(ps,unq,counts):
                 tmp = unqtags==u
@@ -35,7 +39,11 @@ def wordle(matrix,index,top=0.5,count=2309):
                 if min_ == 1:
                     mapping[tuple(index)] = min_
                     return min_
-    mapping[tuple(index)] = min_
-    return min_
+    
+    if min_ == 1000:
+        return wordle(matrix,index,top/1.25,count)
+    else:
+        mapping[tuple(index)] = min_
+        return min_
 
 wordle(matrix,np.arange(2309))
